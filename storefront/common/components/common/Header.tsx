97c92737f98f676ad60af47ca69cb0ac6eda2e59
Phuc-215 <ntphuc23@clc.fitus.edu.vn>
2026-05-24 14:43:35 +0700

    Testing

--- storefront/common/components/common/Header.tsx
+++ storefront/common/components/common/Header.tsx
@@ -121,7 +121,7 @@ const Header = ({ children }: Props) => {
         <nav className="limiter-menu-desktop container">
           {/* <!-- Logo desktop --> */}
           <Link href="/" className="header-logo me-3">
-            <h3 className="text-red">Yas - Storefront Test</h3>
+            <h3 className="text-red">Yas - Storefront Testing</h3>
           </Link>
 
           {/* <!-- Search --> */}
